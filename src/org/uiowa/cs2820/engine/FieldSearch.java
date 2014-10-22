d10167f90b28454f87a5198c0397765cb9b0570a
cgrycki <christopher-grycki@uiowa.edu>
2014-10-22 16:01:38 -0500

    FieldSearch change method getEquals
    - Renamed the method to findEquals to be
      consistent with the "imagined interface"
      provided by the professor.
    - Commented out unimplemented methods
    - Changed method to return an arraylist of
      strings.
    - Changed method to work with update database class

--- src/org/uiowa/cs2820/engine/FieldSearch.java
+++ src/org/uiowa/cs2820/engine/FieldSearch.java
@@ -14,19 +14,19 @@ import org.uiowa.cs2820.engine.Database;
 // getValue - searches for matching values
 
 public class FieldSearch {
-	// Array list of fields to hold results
-	private static List<Field> results;
+	// Array of strings to hold results
+	private ArrayList<String> results;
 	
 	// getEquals searches a database for a field and 
 	// returns a list of fields for which there is an
 	// exact match.
-	public static List<Field> getEquals(Field fd) {
+	public ArrayList<String> findEquals(Field fd) {
 		int index = 0;
 		int size = Database.getSize();
 		
 		while (index < size) {
-			if (Database.get(index) == fd) {
-				results.add(Database.get(index));
+			if ( Database.getFieldByIndex(index) == fd) {
+				results.add(Database.getAddrByIndex(index));
 			}
 		}
 		return results;
@@ -35,16 +35,19 @@ public class FieldSearch {
 	// getKey searches the database for fields which match
 	// the key of the given field and returns the results
 	// in a list
-	public static List<Field> getKey(Field fd) {
+	/*
+	public List<String> getKey(Field fd) {
 		// implement later
 		return results;
 	}
-	
+	*/
 	// getValue searches the database for fields which match
 	// the value of the given field and returns the results
-	// in a list	
-	public static List<Field> getValue(Field fd) {
+	// in a list
+	/*
+	public List<String> getValue(Field fd) {
 		// implement later
 		return results;
-	}	
+	}
+	*/	
 }
