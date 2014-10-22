1ae3f9818c43e88114742966e93de7fa5f3e47d9
Cale Bierman <cale-bierman@uiowa.edu>
2014-10-22 18:48:17 -0500

    added equals method to Field class

--- src/org/uiowa/cs2820/engine/Field.java
+++ src/org/uiowa/cs2820/engine/Field.java
@@ -1,5 +1,6 @@
 package org.uiowa.cs2820.engine;
 
+
 public class Field {
 	String fieldName;
 	String fieldValue;
@@ -34,6 +35,21 @@ public class Field {
 	public void setFieldValue(String fieldValue) {
 		this.fieldValue = fieldValue;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) { return false; }
+		if (!(obj instanceof Field)) {return false;}
+		if (obj == this) { return true; }
+		if (obj.getClass() != this.getClass()) { return false; }
+		
+		Field fieldObj = (Field) obj;
+		
+		return ((this.fieldName.equals(fieldObj.fieldName)) && (this.fieldValue.equals(fieldObj.fieldValue)));
+		
+	}
 }
+
+
 
 
