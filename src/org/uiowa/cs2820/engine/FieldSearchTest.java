f3b67f52548d1773158c541fdb5211c1c7147395
Chris Grycki <christopher-grycki@uiowa.edu>
2014-10-23 19:42:08 -0500

    Fix FieldSearchTest
    Fixed the JUnit test for FieldSearch.
    All tests are currently passing.

--- src/org/uiowa/cs2820/engine/FieldSearchTest.java
+++ src/org/uiowa/cs2820/engine/FieldSearchTest.java
@@ -7,15 +7,18 @@ import java.util.*;
 import org.junit.Test;
 
 public class FieldSearchTest {
-
+	ArrayList<String> results = new ArrayList<String>();
+	ArrayList<String> expectedResults = new ArrayList<String>();
 	// This methods runs one time before any test methods
 	// as a setup
 	@BeforeClass
 	public static void initialSetUp() {
+		// Instantiate Fields
 		Field fd0 = new Field("fruit","bananas,apples,oranges");
 		Field fd1 = new Field("shape","sqaure");
 		Field fd2 = new Field("cheese","parmesan,cheddar");
 		Field fd3 = new Field("dupes","I am a duplicate");
+		// Add the fields to the database
 		Database.add(fd0,"id0");
 		Database.add(fd1,"id1");
 		Database.add(fd2,"id2");
@@ -23,31 +26,39 @@ public class FieldSearchTest {
 		Database.add(fd3,"id4");
 		
 	}
-	
+	// This method runs prior to each and every test
+	@Before
+	public void setUp() {
+		
+	}
+	// This method runs after each and every test
+	@After
+	public void tearDown() {
+		// Empty the ArrayLists so each test can
+		// begin fresh
+		results.clear();
+		expectedResults.clear();
+	}
+	// This test should get zero results
 	@Test
 	public void FindEqualsShouldReturnNothing() {
 		Field fdUnique = new Field("unique","nothing else looks like this field");
-		ArrayList<String> results = new ArrayList<String>();
 		results = FieldSearch.findEquals(fdUnique);
 		assertTrue(results.isEmpty());
 	}
-	
+	// This test should get one result
 	@Test
 	public void FindEqualsShouldReturnOneMatch() {
 		Field fdWillMatch = new Field("shape","sqaure");
-		ArrayList<String> results = new ArrayList<String>();
 		results = FieldSearch.findEquals(fdWillMatch);
-		ArrayList<String> expectedResults = new ArrayList<String>();
 		expectedResults.add("id1");
 		assertEquals(expectedResults,results);
 	}
-	
+	// This test should get two results
 	@Test
 	public void FindEqualsShouldReturnTwoMatches() {
 		Field fdWillMatch = new Field("dupes","I am a duplicate");
-		ArrayList<String> results = new ArrayList<String>();
 		results = FieldSearch.findEquals(fdWillMatch);
-		ArrayList<String> expectedResults = new ArrayList<String>();
 		expectedResults.add("id3");
 		expectedResults.add("id4");
 		assertEquals(expectedResults,results);
