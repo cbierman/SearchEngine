eb224fe8a31ecc065c16889329191e6a54731a5c
hmmnd <raymond-hammond@uiowa.edu>
2014-10-23 21:00:02 -0500

    Update Indexer.java

--- src/org/uiowa/cs2820/engine/Indexer.java
+++ src/org/uiowa/cs2820/engine/Indexer.java
@@ -1,7 +1,5 @@
 package org.uiowa.cs2820.engine;
 
-import java.io.FileReader;
-import java.util.ArrayList;
 
 import org.uiowa.cs2820.engine.Field;
 import org.uiowa.cs2820.engine.Database;
@@ -12,41 +10,33 @@ public class Indexer {
 	private Database myDatabase;
 	private Boolean isClosed;
 	
-	Indexer(String indentifier, Database myDatabase) {
+	Indexer(String indentifier, Database myDatabase) { 
 		this.identifier = identifier;
 		this.myDatabase = myDatabase;
 		this.isClosed = false;
 	}
 	
-	public void add(Field newField){
-		if (!isClosed){
+	public void add(Field newField){		//add a field and store in database
+		if (isClosed == false){
 			myDatabase.add(newField, identifier);}
 	}
+	//public void remove(Field newField){
+	//	if (!isClosed){
+	//		myDatabase.remove(newField);
+	//	}
+	//}
 	
-	public void close(){
+	public void close(){  //close the indexer
 		isClosed = true;
 	}
 	
-	public String getId() { 					//where the identifier can be retrieved
+	public String getId() { 	//where the identifier can be retrieved
 		return this.identifier
 	}
 	
-	public void lineId(String identifier, String line) {
-		ArrayList ent = new ArrayList();
-		String[] sentence = line.split("\\s+"); //takes a line and creates field objects
-		if (sentence[0].length() == 0)
-			return;
-		String fieldName = sentence[0];
-		for (int i=1; i<sentence.length; i++) { //uses the first word as field name and uses others as values
-			Field n = new Field(fieldName, sentence[i]);
-			ent.add(n); //create the [(fieldname,value), ident.] structure
-			ent.add(identifier);
-			Database.addField(n); //add to the database
-		}
-	}
-	public void addField(Field addend) { //adds field to database
-		Database.add(addend);
-		
-		}
+	//public String defaultIdentifier(Field item){
+		//set a default identifier for a field object
+	//}
+	
 	
 }
